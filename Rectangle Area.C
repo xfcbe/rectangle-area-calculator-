cac401d5374ec435203a5e5293eb54ae2f3a6073
xfcbe <73060562+xfcbe@users.noreply.github.com>
2020-11-13 21:03:23 +0300

    Add files via upload

---
+++ Rectangle Area.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int r_Area(int a,int b);
+
+int main()
+{
+    int side;
+    int length;
+
+    printf("Enter side: ");
+    scanf("%d",&side);
+
+    printf(" \n");
+    printf("Enter length ");
+    scanf("%d",&length);
+
+    int calculation = r_Area(side ,length);
+    printf("\nanswer is: %d ",calculation);
+
+    return 0;
+}
+
+int r_Area(int a,int b)
+{
+    int result = a * b;
+    return result;
+}
